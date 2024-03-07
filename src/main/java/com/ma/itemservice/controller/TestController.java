39adc40529511ea5a58d74a012e25a4046ed0c4d
minho <mino.j@sqisoft.com>
2024-03-07 13:32:24 +0900

    CloudConfig client 기본

--- src/main/java/com/ma/itemservice/controller/TestController.java
+++ src/main/java/com/ma/itemservice/controller/TestController.java
@@ -1,15 +1,25 @@
 package com.ma.itemservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/test")
+    @Value("${spring.datasource.url}")
+    private String datasourceUri;
+
+    @GetMapping("/call")
     public String testCall() {
         return "item test";
+    }
+
+    @GetMapping("/data")
+    public String data(){
+        return datasourceUri;
     }
 }
